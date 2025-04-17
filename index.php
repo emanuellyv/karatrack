f9ff7691a7223a3a2d1b18a061c364d67f2b09ad
emanuelly <emanuellyvalenga.dev@gmail.com>
2025-04-17 15:47:20 -0300

    Adicionando o método podeLutar na enum Faixa

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-//require __DIR__ . "/src/Modelo/Karateca.php";
-//require __DIR__ . "/src/Modelo/Faixa.php";
-//require __DIR__ . "/src/Modelo/Dojo.php";
-//require __DIR__ . "/src/Modelo/Aluno.php";
-//require __DIR__ . "/src/Modelo/Sensei.php";
-
 require_once 'vendor/autoload.php';
 
 use KaraTrack\Modelo\Aluno;
@@ -22,4 +16,5 @@ $aluno = new Aluno(
 
 $aluno->validarAno($aluno->getAnoNascimento());
 echo "Aluno: " . $aluno->getNome() . " - " . $aluno->calcularIdade() . " anos" . PHP_EOL;
-//echo "Faixa: " . $aluno->getFaixa() . " - Dojo: " . $aluno->getDojo() . PHP_EOL;
+echo "Faixa: " . $aluno->getFaixa()->name . " - Dojo: " . $aluno->getDojo()->name . PHP_EOL;
+echo "Campeonato de luta: " . $aluno->getFaixa()->podeLutar();
--- src/Modelo/Faixa.php
+++ src/Modelo/Faixa.php
@@ -12,4 +12,15 @@ enum Faixa
     case Marrom;
     case Preta;
     case Mestre;
+
+    public function podeLutar()
+    {
+        if ($this === Faixa::Laranja || $this === Faixa::Verde || $this === Faixa::Roxa || $this === Faixa::Marrom) {
+            return "Pode lutar contra faixas coloridas";
+        } elseif ($this === Faixa::Preta) {
+            return "Pode lutar apenas contra faixas pretas";
+        } elseif ($this === Faixa::Mestre) {
+            return "Pode lutar apenas contra mestres";
+        }
+    }
 }
